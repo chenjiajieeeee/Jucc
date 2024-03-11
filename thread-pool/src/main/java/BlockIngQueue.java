8ae6e52f7f55d17a7303ae779cf767b01148b2cf
chenjiajie <2868203618@qq.com>
2024-03-11 16:53:34 +0800

    feat:阻塞队列实现线程池

---
+++ thread-pool/src/main/java/BlockIngQueue.java
@@ -0,0 +1,165 @@
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+@Slf4j(topic = "c.BlockIngQueue")
+public class BlockIngQueue<T> {
+
+    private Deque<T> deque = new ArrayDeque<>();
+
+    private ReentrantLock lock= new ReentrantLock();
+
+    private Condition fullWartSet=lock.newCondition();
+    private Condition emptyWaitSet=lock.newCondition();
+
+    private int capatity;
+
+    public BlockIngQueue(int capatity){
+        this.capatity=capatity;
+
+    }
+
+    //带超时的阻塞获取
+    public T poll(long timeout, TimeUnit timeUnit){
+        lock.lock();
+        try {
+            long nanos = timeUnit.toNanos(timeout);
+
+            while (deque.isEmpty()){
+                try {
+                    //返回的是剩余的时间
+                    if (nanos<=0)return null;
+                    nanos= emptyWaitSet.awaitNanos(nanos);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+
+            }
+            T t = deque.removeFirst();
+            fullWartSet.signal();
+            return t;
+        }finally {
+            lock.unlock();
+        }
+
+    }
+
+    public T take(){
+        lock.lock();
+        try {
+            while (deque.isEmpty()){
+                try {
+                    emptyWaitSet.await();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+
+            }
+            T t = deque.removeFirst();
+            fullWartSet.signal();
+            return t;
+        }finally {
+            lock.unlock();
+        }
+
+    }
+
+    public void put(T element){
+        lock.lock();
+        try {
+            while (deque.size()==capatity){
+                try {
+                    fullWartSet.await();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+
+            }
+            deque.addLast(element);
+            emptyWaitSet.signal();
+        }finally {
+            lock.unlock();
+        }
+    }
+    public int size(){
+        lock.lock();
+        try {
+            return deque.size();
+        }finally {
+            lock.unlock();
+        }
+    }
+}
+
+@Slf4j(topic = "c.BlockIngQueue")
+class ThreadPool{
+    private BlockIngQueue<Runnable> blockIngQueue;
+    private HashSet<Worker> workers=new HashSet<>();
+
+    private int coreSize;
+    private long timeOut;
+
+    private TimeUnit timeUnit;
+
+    public void execute(Runnable task){
+        //根据当前任务队列数量进行处理
+        //超过coresize时，加入任务队列暂存
+        synchronized (workers) {
+            if (workers.size() < coreSize) {
+
+                Worker worker = new Worker(task);
+                log.debug("新增一个worker{},{}",worker,task);
+                workers.add(worker);
+                worker.start();
+            } else {
+                log.debug("加入任务队列{}",task);
+                blockIngQueue.put(task);
+            }
+        }
+
+    }
+
+    public ThreadPool(int coreSize,long timeOut,TimeUnit timeUnit,int queueCapatity){
+        this.coreSize=coreSize;
+        this.timeOut=timeOut;
+        this.timeUnit=timeUnit;
+        this.blockIngQueue=new BlockIngQueue<>(queueCapatity);
+    }
+
+    class Worker extends Thread{
+        private Runnable task;
+
+        public Worker(Runnable task){
+            this.task=task;
+        }
+
+        @Override
+        public void run(){
+            //当task不为空，执行任务
+            //当task执行完毕，再从阻塞队列中获取值
+            while (task!=null||(task=blockIngQueue.poll(timeOut,timeUnit))!=null ){
+                try {
+                    log.debug("正在执行。。。{}",task);
+                    task.run();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    task=null;
+                }
+            }
+            synchronized(workers){
+                log.debug("woker{}被移除",this);
+                workers.remove(this);
+            }
+        }
+
+
+    }
+}
+
+
+
---
+++ thread-pool/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j(topic = "c.Main")
+public class    Main {
+    public static void main(String[] args) {
+        ThreadPool threadPool=new ThreadPool(2,1000, TimeUnit.MILLISECONDS,10);
+
+
+            for(int i=0;i<5;i++){
+                final int j=i;
+                threadPool.execute(()->{
+                    log.debug("{}",j);
+                });
+            }
+
+
+
+    }
+}
